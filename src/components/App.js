54e40a1d9397cb5e28156f215d83444ee6713a2d
Ncumic <Ncumic00@gmail.com>
2023-12-02 12:58:33 -0800

    imported searchFrom into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import react from 'react';
 import {LoginForm} from './login.js';
 import {Post} from './post.js';
 import {Home} from './loggedIn';
+import {searchForm} from './search.js'
 
 function App(props) {
   return (

0737e410988971ac21eb6876ed551ede0fee3e93
Ncumic <ncumic00@gmail.com>
2023-12-07 18:29:09 -0800

    userData.json file added

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,7 @@ function App(props) {
           <searchForm /> 
           <Post/>
           <Home/>
-          
-        {/* <DetailPage /> */}
+
         {/* <SearchPage /> */}
         {/* <AboutPage /> */}
       </div>

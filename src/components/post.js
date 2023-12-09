9274b0a965194ce1a5b9f69df327c922ea1aa843
Ethan Wang <etzwang@uw.edu>
2023-12-09 15:47:48 -0800

    fixed index.js

--- src/components/post.js
+++ src/components/post.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { loginForm } from 'CreatePost.js'
 
 
 export function Post (props) {
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCRd3KQ29iqBS2GJ4Cz4lTBvg043skbr0w",

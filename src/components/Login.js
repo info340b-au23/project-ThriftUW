40cdde0c8061761f876aeb4162947d81d8be929e
Ethan Wang <etzwang@uw.edu>
2023-12-09 17:43:44 -0800

    login.js firebase auth added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,1 +1,37 @@
+import React from 'react';
 
+import { Navigate } from 'react-router-dom';
+import { getAuth, EmailAuthProvider, GoogleAuthProvider } from 'firebase/auth';
+import { StyledFirebaseAuth } from 'react-firebase';
+
+export default function SignInPage(props) {
+    const currentUser = props.currentUser;
+
+    const auth = getAuth();
+
+    const configObbj = {
+        signInOptions: [
+            { provider: EmailAuthProvider.PROVIDER_ID, requiredDisplayName: true},
+            { provider: GoogleAuthProvider.PROVIDER_ID }
+        ],
+        signInFlow: 'popup',
+        callbacks: {
+            signInSuccessWithAuthResult: () => false
+        },
+        credentialHelper: 'none'
+    }
+
+    if (currentUser.userId) {
+        window.alert("Signed in!");
+        return <Navigate to="/" />
+    }
+
+    return (
+        <div className="card bg-light">
+            <div className="container card-body">
+                <StyledFirebaseAuth firebase={auth} uiConfig={configObj} />
+            </div>
+        </div>
+    )
+        
+}

a6ded1a4da761bd21f3a5bfdd9f6b9119b0a3f9a
Ncumic <Ncumic00@gmail.com>
2023-12-10 12:13:47 -0800

    Added changes to Navbar.js, added links structure code

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+          <div>
+            <h1>Out-Of-Fits</h1>
+            <h2>Your UW Campus Thrift Community & Marketplace</h2>
+          </div>
+          <nav>
+            <input type="checkbox" id="nav-toggle" className="nav-toggle" />
+            <label htmlFor="nav-toggle" className="dropdown-btn">
+              &#9776; Menu
+            </label>
+            <ul>
+                <li><Link to="/loggedIn.js">Main</Link></li>
+                <li><Link to="/search.js">Search</Link></li>
+                <li><Link to="/post.js">Posts</Link></li>
+            </ul>
+         </nav>
+    );
+}
+export default Navbar;

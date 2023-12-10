69ebe7ab3835afdfd1a4a533b5fcc1f28efc3857
Ncumic <ncumic00@gmail.com>
2023-12-10 02:22:56 -0800

    Created routes in App.js, and added a navbar file to code in later

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,26 @@
 import react from 'react';
 import {LoginForm} from './Login.js';
 import {Post} from './post.js';
-import {Home} from './loggedIn';
-import {searchForm} from './search.js'
+import {Home} from './loggedIn.js';
+import {searchForm} from './search.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App(props) {
   return (
-      <div>
-          <LoginForm/>
-          <searchForm /> 
-          <Post/>
-          <Home/>
-
-        {/* <SearchPage /> */}
-        {/* <AboutPage /> */}
-      </div>
-    );
+    <Router>
+        <div>
+             <Navbar />
+             <main>
+                <Routes>
+                  <Route path="/Login.js" element={<LoginForm/>} />
+                  <Route path="/loggedIn.js" element={<Home />} />
+                  <Route path="/search.js" element={<searchForm />} />
+                  <Route path="/post.js" element={<Post />} />
+                </Routes>
+             </main>
+        </div>
+    </Router>
+  );
 }
 
 export default App;

30a8b3c1c9f8331394f4e4dcb0de4a9fe0d025f6
Ethan Wang <etzwang@uw.edu>
2023-12-09 17:29:11 -0800

    removed functions.js

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,97 +1,97 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { TextField, Grid } from '@material-ui/core';
 
 
 function loginForm() {
-    const theme = useTheme();
-
     const [formData, setFromData] = useState({
         firstName: '',
         lastName: '',
         email: '',
         message: '',
     });
-}
 
-const [errors, setErrors] = useState({});
-const [isAlertOpen, setAlertOpen] = useState(false);
+    const [errors, setErrors] = useState({});
+    const [isAlertOpen, setAlertOpen] = useState(false);
 
-const handleInputChange = (e) => {
-    const {name, value} = e.target;
-    setFormData({
-        ...formData,
-        [name]: value,
-    });
-};
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
 
-const handleSubmit = (e) => {
-    e.preventDefault();
-    const validationErrors = {};
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const validationErrors = {};
 
-    if (!formData.firstName.trim()) {
-        validationErrors.firstName = 'First name is required';
-    }
-    if (!formData.lastName.trim()) {
-        validationErrors.lastName = 'Last name is required';
-    }
-    if (!formData.email.trim()) {
-        validationErrors.email = 'Email is required';
-    }
-    if (!formData.message.trim()) {
-        validationErrors.message = 'Message is required';
-    }
+        if (!formData.firstName.trim()) {
+            validationErrors.firstName = 'First name is required';
+        }
+        if (!formData.lastName.trim()) {
+            validationErrors.lastName = 'Last name is required';
+        }
+        if (!formData.email.trim()) {
+            validationErrors.email = 'Email is required';
+        }
+        if (!formData.message.trim()) {
+            validationErrors.message = 'Message is required';
+        }
 
-    if (Object.keys (validationErrors). length === 0) {
-        const db = getDatabase();
+        if (Object.keys (validationErrors). length === 0) {
+            const db = getDatabase();
     
-        const usersRef = ref(db,'users');
+            const usersRef = ref(db,'users');
     
-        firebasePush(usersRef, formData)
-            .then(() => {
-                setAlertOpen(true);
-                setFormData({
-                    firstName: '',
-                    lastName: '',
-                    email: '',
-                    message: '',
+            firebasePush(usersRef, formData)
+                .then(() => {
+                    setAlertOpen(true);
+                    setFormData({
+                        firstName: '',
+                        lastName: '',
+                        email: '',
+                        message: '',
+                    });
+                })
+                .catch((error) => {
+                    console.error("Error writing to Firebase Database", error);
                 });
-            })
-            .catch((error) => {
-                console.error("Error writing to Firebase Database", error);
-            });
-    } else {
-        setErrors(validationErrors);
-    }
-};
+            console.log('Form data:', formData);
+        } else {
+            setErrors(validationErrors);
+        }
+    };
 
-return (
-    <form style={formStyles} onSubmit={handleSubmit}>
-        <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-                <TextField
-                    label="First Name"
-                    name="firstName"
-                    variant="outlined"
-                    fullWidth
-                    required
-                    value={formData.firstName}
-                    onChange={handleInputChange}
-                    error={!!errors.firstName}
-                    helperText={errors.firstName}
-                />
+    return (
+        <form style={formStyles} onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        label="First Name"
+                        name="firstName"
+                        variant="outlined"
+                        fullWidth
+                        required
+                        value={formData.firstName}
+                        onChange={handleInputChange}
+                        error={!!errors.firstName}
+                        helperText={errors.firstName}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        label="First Name"
+                        name="firstName"
+                        variant="outlined"
+                        fullWidth
+                        required
+                        value={formData.firstName}
+                        onChange={handleInputChange}
+                        error={!!errors.firstName}
+                        helperText={errors.firstName}
+                    />
+                </Grid>
             </Grid>
-            <Grid item xs={12} sm={6}>
-                <TextField
-                    label="First Name"
-                    name="firstName"
-                    variant="outlined"
-                    fullWidth
-                    required
-                    value={formData.firstName}
-                    onChange={handleInputChange}
-                    error={!!errors.firstName}
-                    helperText={errors.firstName}
-                />
-            </Grid>
-        </Grid>
-    </form>
-)
+        </form>
+    );
+}

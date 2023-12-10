debd0d1547101e6265b7d83d026730f95f899dee
Ethan Wang <etzwang@uw.edu>
2023-12-10 14:40:54 -0800

    changed login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,34 +1,44 @@
-import React, { useState } from 'react';
-import { Navigate } from 'react-router-dom';
-import { getAuth, EmailAuthProvider, GoogleAuthProvider } from 'firebase/auth';
-import { StyledFirebaseAuth } from 'react-firebaseui';
+import React from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DEFAULT_USERS from '../data/users.json';
 
 export default function SignInPage(props) {
     const currentUser = props.currentUser;
+    const loginFunction = props.loginCallback;
 
-    const auth = getAuth();
+    const handleClick = (event) {
+        const whichUser = event.currentTarget.name
+        console.log(whichUser);
+        const selectedUserObj = DEFAULT_USERS.filter((userObj) => userObj.userId === whichUser)[0] || DEFAULT_USERS[0]
 
-    const configObbj = {
-        signInOptions: [
-            { provider: EmailAuthProvider.PROVIDER_ID, requiredDisplayName: true},
-            { provider: GoogleAuthProvider.PROVIDER_ID }
-        ],
-        signInFlow: 'popup',
-        callbacks: {
-            signInSuccessWithAuthResult: () => false
-        },
-        credentialHelper: 'none'
+        loginFunction(selectedUserObj)
     }
 
-    if (currentUser.userId) {
-        window.alert("Signed in!");
-        return <Navigate to="/" />
-    }
+    const userButtons = DEFAULT_USERS.map((userObj) => {
+        if (userObj.userId === currentUser.userId){
+            return null;
+        }
+        return (
+            <Dropdown.Item className="user-icon" key={userObj.userName}
+                name={userObj.userId} onClick={handleClick}
+            >
+                <img src={userObj.userImg} alt={userObj.userName} + " avatar" />
+                {userObj.userName}
+            </Dropdown.Item>
+        )
+    })
 
     return (
         <div className="card bg-light">
             <div className="container card-body">
-                <StyledFirebaseAuth firebase={auth} uiConfig={configObj} />
+                <Dropdown>
+                    <Dropdown.Toggle variant="light">
+                        <img src={currentUser.userImg} alt={currentUser.userName + " avatar"} />
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        {userButtons}
+                    </Dropdown.Menu>
+                </Dropdown>
             </div>
         </div>
     )

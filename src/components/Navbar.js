0ed864036195490f7b55fb86c382732ccc76ff7f
Tim Carlson <timca@arenasports.net>
2023-12-11 13:04:14 -0800

    updated navbar to straight bootstrap. added link stuff

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
+import { Link, NavLink } from 'react-router-dom';
 
 export function Navbar() {
+
+
   return (
     <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/main">Main</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="search">Search</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="post">Post</NavLink>
+              </li>
 
-    <div>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      {/* <div>
         <h1>Out-Of-Fits</h1>
         <h2>Your UW Campus Thrift Community & Marketplace</h2>
     </div>
@@ -15,13 +37,13 @@ export function Navbar() {
         <Container>
           <Navbar.Brand href="/loggedIn.js">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/main.js">Main</Nav.Link>
-            <Nav.Link href="/search.js">Search</Nav.Link>
-            <Nav.Link href="/post.js">Post</Nav.Link>
+            <NavLink to="/main"> Main </NavLink>
+            <NavLink to="/search">Search </NavLink>
+            <NavLink to="/post"> Post </NavLink>
+
           </Nav>
         </Container>
-      </Navbar>
+      </Navbar> */}
     </>
   );
 }
-//export default Navbar;
